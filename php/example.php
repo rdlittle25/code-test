9bd0d9226e0a75e6653b3d8452be3a53196b22b4
Dave Lowther <dave@davelowther.net>
2018-12-06 10:15:45 -0500

    wrap handler in try catch

--- php/example.php
+++ php/example.php
@@ -1,29 +1,31 @@
 <?php
 
-function output()
-{
-    $group = [
-        "testOne" => 1,
-        "testTwo" => [ two => 2, ],
-        "testThree" => [3,]
-        "testFour" => FOUR,
-    ]
-
-    foreach ($group as $key => value) {
-        echo '{$key}: {$value}\n'
-    }
-
-    $output = json_encode($group);
-    if (($jsonError = json_last_error()) > 0) {
-        throw new Exception('Invalid JSON Output');
-    }
-}
-
 try {
+    function output()
+    {
+        $group = [
+            "testOne" => 1,
+            "testTwo" => [ two => 2, ],
+            "testThree" => [3,]
+            "testFour" => FOUR,
+        ]
+
+        foreach ($group as $key => value) {
+            echo '{$key}: {$value}\n'
+        }
+
+        $output = json_encode($group);
+        if (($jsonError = json_last_error()) > 0) {
+            throw new Exception('Invalid JSON Output');
+        }
+    }
+
     $test = output();
 
     // Output to standard out.
     fwrite();
-} catch (Exception $e) {
-    error_log($e, 0, 'php://stdout');
+} catch (Throwable $e) {
+    $class = get_class($e);
+    $msg = "{$class} ({$e->getCode()}): {$e->getMessage()} at {$e->getLine()}";
+    error_log($msg, 0, 'php://stdout');
 }
